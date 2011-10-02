bccc99ce48cf3dafd9964fcddbb17bdd4476cffe
f0rtr3ss <f0rtr3ss.s3cur1ty@gmail.com>
2011-10-01 23:37:56 -0700

    Coding has started... going slowly?

--- Bathroom.c
+++ Bathroom.c
@@ -2,5 +2,43 @@
 
 int main(int argc, char** argv)
 {
-	/* Codez go here */
+	
+}
+
+int NewStudent(Student* stud, int id, const char* first, const char* last)
+{
+	Name* namep;
+	
+	if((stud = malloc(sizeof(Student))) == NULL)
+		return -ENOMEM;
+	
+	namep = NewName(strlen(first), strlen(last));
+	
+	if(namep == NULL)
+		return -ENOMEM;
+	
+	strcpy(namep->first, first);
+	strcpy(namep->last, last);
+	
+	stud->name = *namep;
+	ReleaseName(&namep);
+	
+	stud->id = id;
+}
+
+Name* NewName(int first, int last)
+{
+	if(first < 0 || last < 0)
+		return NULL;
+	
+	
+}
+
+void ReleaseName(Name** namepp)
+{
+	free(*(namepp)->first);
+	*(namepp)->first = NULL;
+	free(*(namepp)->last);
+	*(namepp)->last = NULL;
+	*namepp = NULL;
 }
--- Bathroom.h
+++ Bathroom.h
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
-typedef struct student {
-	int id,
-} Student;
+struct personname {
+	char* first;
+	char*  last;
+};
+
+struct date {
+	unsigned int day;
+	unsigned int month;
+	unsigned int year;
+};
+
+typedef struct personame Name;
+
+struct student {
+	int id;
+	Name name;
+	unsigned char remaining;
+	date last_trip;
+};
+
+typedef struct student Student;
+typedef Student QueueEntry;
+
+int NewStudent(int, const char*, const char*);
+Name* NewName(int, int);
+
